f97c148271180fdc21a3730a46ade1c45ed6452b
Mukesh Sahu <sahumukesh820@gmail.com>
2026-06-29 14:49:24 +0530

    practiced the airethmetic operators

---
+++ Learning/Python basics/airethmetic_operators.py
@@ -0,0 +1,18 @@
+a = 11
+b = 3
+
+summation = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+floor_division = a // b
+modular = a % b
+exponent = a ** b
+
+print("summation :", summation)
+print("Subtraction :", subtraction)
+print("Multiplication :", multiplication)
+print("Division :", division)
+print("Floor Division :", floor_division)
+print("Modular :", modular)
+print("Exponent :", exponent)
