2abc75f182c0096c1ff1f7a796fa7939b7b74135
Mukesh Sahu <sahumukesh820@gmail.com>
2026-06-29 13:50:29 +0530

    practiced variables and data types

---
+++ Learning/Python basics/variables.py
@@ -0,0 +1,15 @@
+name = "Mukesh Sahu"
+age = 30
+city = "Surat"
+role = "AI Engineer"
+
+print("My name is",name)
+print("I am", age, "years old")
+print("I live in", city)
+print("I am working as", role)
+
+
+print(type(name))
+print(type(age))
+print(type(city))
+print(type(role))
