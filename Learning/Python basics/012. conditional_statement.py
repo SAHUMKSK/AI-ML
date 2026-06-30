96a5c54b3b1c0a8493ddc835f890e437c2701c41
Mukesh Sahu <sahumukesh820@gmail.com>
2026-06-30 15:48:46 +0530

    Learned the conditional statement

---
+++ Learning/Python basics/012. conditional_statement.py
@@ -0,0 +1,31 @@
+'''
+Conditional statement: It allows to execute the different blocks of code based on the condition.
+
+it has 3 types:
+1. simple if
+2. if else
+3. if elif else
+
+'''
+# simple if:
+age = 18
+if age >= 18:
+    print("You are allowed to vote.")
+
+
+# if else:
+marks = 33
+if marks >= 35:
+    print("You are pass.")
+else:
+    print("You are fail.")
+
+# if elif else:
+number = 10
+
+if number > 0:
+    print("Number is positive.")
+elif number < 0:
+    print("Number is negative.")
+else:
+    print("Number is zero.")
