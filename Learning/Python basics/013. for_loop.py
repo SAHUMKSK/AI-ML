cf374117e7b48d339be0d1b4d6d3dfbc15ace172
Mukesh Sahu <sahumukesh820@gmail.com>
2026-06-30 17:59:38 +0530

    Learned the for loop

---
+++ Learning/Python basics/013. for_loop.py
@@ -0,0 +1,24 @@
+'''
+Loop is to used to repeat the block of code for a specific time.
+
+syntax:
+for variable in iterable:
+    block of code
+
+for variable in range(start, stop, step): except stop rest of the things are optional(default step = 1)
+    block of code
+
+    
+'''
+
+for i in range(1, 11, 1): # start = 0, stop = 11, step = 1 
+    print(i) # It will print the
+
+
+print("Print only odd numbers upto 11")
+for j in range(1, 11, 2):
+    print(j)
+
+
+for k in enumerate("MUKESH SAHU"):# In k it stores the index and value
+    print(k)
