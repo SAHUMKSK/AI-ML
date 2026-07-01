f62400dae8ebe6d625afe48f969b6056ca9afdbb
Mukesh Sahu <sahumukesh820@gmail.com>
2026-07-01 21:38:16 +0530

    Learned the how to take input

---
+++ Learning/Python basics/016. input.py
@@ -0,0 +1,16 @@
+'''
+Input: Take input from users.
+
+Syntax:
+variable_name = input("Print message")
+
+name = input("Enter your name")
+
+'''
+name = input("Enter your name: ")
+designation = input("Enter your designation: ")
+age = input("Enter your age: ")
+
+print("Hello", name + ", Good morning!")
+print("Your designation is", designation + ".")
+print("You are", age , "years old.")
