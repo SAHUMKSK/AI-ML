61992a5f227abfa10330c767845e0588ae3e3210
Mukesh Sahu <sahumukesh820@gmail.com>
2026-07-01 08:53:47 +0530

    Learned the while loop

---
+++ Learning/Python basics/014. while_loop.py
@@ -0,0 +1,20 @@
+'''
+While loop is used to repeat a block of code over and over as long as specific condition stays true.
+
+syntax:
+While ():
+    block of code
+
+while ():
+    block of code
+
+    
+'''
+
+x = 1
+
+print("It will print 1 to 10.")
+
+while (x < 11): # The inner block of code will execute until the condition stays true.
+    print(x)
+    x +=1 # In while loop we have to meanually increement or decreement.
