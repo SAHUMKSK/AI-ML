5719db51bdf4879dd6a07c56965e5f5c989f5148
Mukesh Sahu <sahumukesh820@gmail.com>
2026-07-01 10:56:54 +0530

    Learned the for control statements

---
+++ Learning/Python basics/015. loop_controls.py
@@ -0,0 +1,36 @@
+'''
+Loop controls: it is used to conntrol the loops.
+
+1. Break: It will terminates the loop entirely.
+
+2. Continue: It skips the remaining current iteration.
+
+'''
+
+print("Break")
+for i in range(1, 11):
+    
+    if i == 6:
+        break
+    print(i)
+
+print("The loop has been terminated.")
+
+print("Continue")
+for i in range(1, 11):
+    
+    if i == 6:
+        continue # Here it will skip this iteration
+    
+    print(i)
+
+    
+
+for i in range(1, 6):
+    for j in range(1, 6):
+        if i == 2 and j == 2:
+            print("It terminates the loop.")
+            break
+        print(i, j)
+        print("Inner loop")
+    print("Outer loop")
