351527cbaee1e64978dbf8f6066732b8eeca1947
Mukesh Sahu <sahumukesh820@gmail.com>
2026-07-01 08:52:22 +0530

    Learned the for loop

--- Learning/Python basics/013. for_loop.py
+++ Learning/Python basics/013. for_loop.py
@@ -1,5 +1,6 @@
 '''
 Loop is to used to repeat the block of code for a specific time.
+For loop is used to iterate over a sequence(such as list, tuple, or string).
 
 syntax:
 for variable in iterable:
@@ -8,7 +9,6 @@ for variable in iterable:
 for variable in range(start, stop, step): except stop rest of the things are optional(default step = 1)
     block of code
 
-    
 '''
 
 for i in range(1, 11, 1): # start = 0, stop = 11, step = 1 
@@ -20,5 +20,5 @@ for j in range(1, 11, 2):
     print(j)
 
 
-for k in enumerate("MUKESH SAHU"):# In k it stores the index and value
+for k in enumerate("MUKESH SAHU"):# In k it stores the index and value 
     print(k)
